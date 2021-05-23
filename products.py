936b9fa5c9043ebfac0cbd5484cddf74302153b5
Aiden Jhang <ssjhang0510@gmail.com>
2021-05-23 22:13:37 +0800

    final version

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	p = [name, price]
 	products.append(p) 
 print(products) #二維清單
@@ -13,3 +14,9 @@ products[0][0]
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+
+with open('product.csv', 'w') as f: #utf-8是最通用的編碼
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
